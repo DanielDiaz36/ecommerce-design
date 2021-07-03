791dd0ef871d1d04b76d6ee593bb19355651012e
daniel <daniel@outboss.io>
2021-07-03 19:35:59 -0400

    delete STATICFILES_STORAGE

--- app/settings.py
+++ app/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'website', 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Modelo User personalizado
